9f60da32148fded1df1ed7f068847d2f8b41f519
Pierre Lundström <pierre.lundstrom@learnet.se>
2020-11-10 19:14:36 +0100

    lagt till fler metoder i db manager

--- src/models/DBManager.java
+++ src/models/DBManager.java
@@ -27,6 +27,28 @@ public class DBManager {
             }
         }
     }
+
+    public boolean executeUpdate(DB db, String query){
+        Statement stmt= null;
+        try {
+            stmt = db.getConnection().createStatement();
+            if (query.contains("password")){
+                System.out.println("Executing query: " + "Password protected");
+            }else {
+                System.out.println("Executing query: " + query);
+            }
+            stmt.executeUpdate(query);
+            if (query.contains("password")) {
+                System.out.println("Successfully Executed Query: " + "Password protected");
+            } else {
+                System.out.println("Successfully Executed Query: " + query);
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
     
     public boolean executeUpdate(String query){
         Statement stmt= null;
@@ -48,6 +70,28 @@ public class DBManager {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public ResultSet executeQuery(DB db, String query){
+        Statement stmt= null;
+        try {
+            stmt = db.getConnection().createStatement();
+            if (query.contains("password")){
+                System.out.println("Executing query: " + "Password protected");
+            }else {
+                System.out.println("Executing query: " + query);
+            }
+            ResultSet rs=stmt.executeQuery(query);
+            if (query.contains("password")) {
+                System.out.println("Successfully Executed Query: " + "Password protected");
+            } else {
+                System.out.println("Successfully Executed Query: " + query);
+            }
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public ResultSet executeQuery(String query){
@@ -81,6 +125,29 @@ public class DBManager {
         this.db = db;
     }
 
+    public int deleteAll(DB db, String table){
+        Statement stmt= null;
+        try {
+            stmt = db.getConnection().createStatement();
+            String query = "DELETE from " + table;
+            if (query.contains("password")){
+                System.out.println("Executing query: " + "Password protected");
+            }else {
+                System.out.println("Executing query: " + query);
+            }
+            int deletedRows = stmt.executeUpdate(query);
+            if (query.contains("password")) {
+                System.out.println("Successfully Executed Query: " + "Password protected");
+            } else {
+                System.out.println("Successfully Executed Query: " + query);
+            }
+            return deletedRows;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
     public int deleteAll(String table){
         Statement stmt= null;
         try {
@@ -102,6 +169,45 @@ public class DBManager {
             e.printStackTrace();
         }
         return -1;
+    }
+
+    public boolean delete(DB db, String table, ArrayList<String> filtercolumns, ArrayList<String> filtervalues){
+        Statement stmt = null;
+        try {
+            stmt = (Statement) db.getConnection().createStatement();
+            String query = "delete from " + table + " where ";
+
+            for (int i = 0; i < filtercolumns.size(); i++){
+                if (filtercolumns.get(i) != null){
+                    if (i == 0){
+                        if (i == (filtercolumns.size() - 1)){
+                            query += filtercolumns.get(i) + " = '" + filtervalues.get(i) + "'";
+                        }else {
+                            query += filtercolumns.get(i) + " = '" + filtervalues.get(i);
+                        }
+                    }else if (i < (filtercolumns.size() - 1)){
+                        query += "' and " + filtercolumns.get(i) + " = '" + filtervalues.get(i);
+                    }else if (i == (filtercolumns.size() - 1)){
+                        query += "' and " + filtercolumns.get(i) + " = '" + filtervalues.get(i) + "'";
+                    }
+                }
+            }
+            if (query.contains("password")){
+                System.out.println("Executing query: " + "Password protected");
+            }else {
+                System.out.println("Executing query: " + query);
+            }
+            stmt.executeUpdate(query);
+            if (query.contains("password")) {
+                System.out.println("Successfully Executed Query: " + "Password protected");
+            } else {
+                System.out.println("Successfully Executed Query: " + query);
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public boolean delete(String table, ArrayList<String> filtercolumns, ArrayList<String> filtervalues){
@@ -143,11 +249,72 @@ public class DBManager {
         return false;
     }
 
+    public ResultSet selectAll(DB db, String table){
+        Statement stmt= null;
+        try {
+            stmt = db.getConnection().createStatement();
+            String query = "select * from " + table;
+            if (query.contains("password")){
+                System.out.println("Executing query: " + "Password protected");
+            }else {
+                System.out.println("Executing query: " + query);
+            }
+            ResultSet rs=stmt.executeQuery(query);
+            if (query.contains("password")) {
+                System.out.println("Successfully Executed Query: " + "Password protected");
+            } else {
+                System.out.println("Successfully Executed Query: " + query);
+            }
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public ResultSet selectAll(String table){
         Statement stmt= null;
         try {
             stmt = db.getConnection().createStatement();
             String query = "select * from " + table;
+            if (query.contains("password")){
+                System.out.println("Executing query: " + "Password protected");
+            }else {
+                System.out.println("Executing query: " + query);
+            }
+            ResultSet rs=stmt.executeQuery(query);
+            if (query.contains("password")) {
+                System.out.println("Successfully Executed Query: " + "Password protected");
+            } else {
+                System.out.println("Successfully Executed Query: " + query);
+            }
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ResultSet selectAllWhere(DB db, String table, ArrayList<String> filtercolumns, ArrayList<String> filtervalues){
+        Statement stmt= null;
+        try {
+            stmt = db.getConnection().createStatement();
+            String query = "select * from " + table + " where ";
+            for (int i = 0; i < filtercolumns.size(); i++){
+                if (filtercolumns.get(i) != null){
+                    if (i == 0){
+                        if ((filtercolumns.size() - 1) == i) {
+                            query += filtercolumns.get(i) + " = '" + filtervalues.get(i) + "'";
+                        }else{
+                            query += filtercolumns.get(i) + " = '" + filtervalues.get(i);
+                        }
+                    }else if (i < (filtercolumns.size() - 1)){
+                        query += "' and " + filtercolumns.get(i) + " = '" + filtervalues.get(i);
+                    }else if (i == (filtercolumns.size() - 1)){
+                        query += "' and " + filtercolumns.get(i) + " = '" + filtervalues.get(i) + "'";
+                    }
+                }
+            }
             if (query.contains("password")){
                 System.out.println("Executing query: " + "Password protected");
             }else {
@@ -183,6 +350,44 @@ public class DBManager {
                         query += "' and " + filtercolumns.get(i) + " = '" + filtervalues.get(i);
                     }else if (i == (filtercolumns.size() - 1)){
                         query += "' and " + filtercolumns.get(i) + " = '" + filtervalues.get(i) + "'";
+                    }
+                }
+            }
+            if (query.contains("password")){
+                System.out.println("Executing query: " + "Password protected");
+            }else {
+                System.out.println("Executing query: " + query);
+            }
+            ResultSet rs=stmt.executeQuery(query);
+            if (query.contains("password")) {
+                System.out.println("Successfully Executed Query: " + "Password protected");
+            } else {
+                System.out.println("Successfully Executed Query: " + query);
+            }
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ResultSet selectColumn(DB db, String table, ArrayList<String> columns){
+        Statement stmt= null;
+        try {
+            stmt = db.getConnection().createStatement();
+            String query = "select ";
+            for (int i = 0; i < columns.size(); i++){
+                if (columns.get(i) != null){
+                    if (i == 0) {
+                        if (i == (columns.size() - 1)){
+                            query += columns.get(i) + " from " + table;
+                        }else {
+                            query += columns.get(i);
+                        }
+                    }else if (i < (columns.size() - 1)){
+                        query += ", " + columns.get(i);
+                    } else if (i == (columns.size() - 1)){
+                        query += ", " + columns.get(i) + " from " + table;
                     }
                 }
             }
@@ -240,6 +445,61 @@ public class DBManager {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public boolean edit(DB db, String table, ArrayList<String> filtercolumns, ArrayList<String> filtervalues, ArrayList<String> setcolumns, ArrayList<String> setvalues){
+        Statement stmt = null;
+        try {
+            stmt = db.getConnection().createStatement();
+            String query = "update " + table + " set ";
+
+            for (int i = 0; i < setcolumns.size(); i++){
+                if (setcolumns.get(i) != null){
+                    if (i == 0){
+                        if ((setcolumns.size() - 1) == i) {
+                            query += setcolumns.get(i) + " = '" + setvalues.get(i) + "'";
+                        }else{
+                            query += setcolumns.get(i) + " = '" + setvalues.get(i);
+                        }
+                    }else if (i < (setcolumns.size() - 1)){
+                        query += "' and " + setcolumns.get(i) + " = '" + setvalues.get(i);
+                    }else if (i == (setcolumns.size() - 1)){
+                        query += "' and " + setcolumns.get(i) + " = '" + setvalues.get(i) + "'";
+                    }
+                }
+            }
+            query += " where ";
+            for (int i = 0; i < filtercolumns.size(); i++){
+                if (filtercolumns.get(i) != null){
+                    if (i == 0){
+                        if ((filtercolumns.size() - 1) == i) {
+                            query += filtercolumns.get(i) + " = '" + filtervalues.get(i) + "'";
+                        }else{
+                            query += filtercolumns.get(i) + " = '" + filtervalues.get(i);
+                        }
+                    }else if (i < (filtercolumns.size() - 1)){
+                        query += "' and " + filtercolumns.get(i) + " = '" + filtervalues.get(i);
+                    }else if (i == (filtercolumns.size() - 1)){
+                        query += "' and " + filtercolumns.get(i) + " = '" + filtervalues.get(i) + "'";
+                    }
+                }
+            }
+            if (query.contains("password")){
+                System.out.println("Executing query: " + "Password protected");
+            }else {
+                System.out.println("Executing query: " + query);
+            }
+            stmt.executeUpdate(query);
+            if (query.contains("password")) {
+                System.out.println("Successfully Executed Query: " + "Password protected");
+            } else {
+                System.out.println("Successfully Executed Query: " + query);
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public boolean edit(String table, ArrayList<String> filtercolumns, ArrayList<String> filtervalues, ArrayList<String> setcolumns, ArrayList<String> setvalues){
